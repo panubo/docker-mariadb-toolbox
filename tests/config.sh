717428f34497b398fd8d433199e423bc45555cff
Andrew Cutler <andrew@panubo.com>
2020-11-07 17:59:46 +1100

    make dind image configurable

--- tests/config.sh
+++ tests/config.sh
@@ -4,3 +4,5 @@ TEST_TAG='test'
 TEST_CONTAINER="${TEST_IMAGE}:${TEST_TAG}"
 MARIADB_IMAGE='mariadb'
 MARIADB_TAG='latest'
+DIND_IMAGE='jpetazzo/dind'
+DIND_TAG='latest'
--- tests/dind-runner.sh
+++ tests/dind-runner.sh
@@ -4,13 +4,17 @@ set -e
 
 [ "$DEBUG" == 'true' ] && set -x
 
+CWD="$(dirname $0)/"
+
+. ${CWD}functions.sh
+
 TEST_CONTAINER="dind-runner-$$"
 DOCKERFILE="Dockerfile.test"
 
 echo ">> Using Temp Dockerfile: $DOCKERFILE"
 
 cat << EOF > $DOCKERFILE
-FROM jpetazzo/dind:latest
+FROM ${DIND_IMAGE}:${DIND_TAG}
 ADD .  /build/
 WORKDIR /build
 CMD ["/build/tests/runner.sh"]
